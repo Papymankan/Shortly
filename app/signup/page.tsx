24ea4edc32ae474e83d8d0c2cef295500a912437
Papymankan <parsa84rostami@gmail.com>
2025-07-30 12:18:02 +0330

    add signup logic

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import Footer from "@/components/Footer";
+import SignupForm from "@/components/SignupForm";
 
 export default function page() {
   return (
@@ -17,66 +18,7 @@ export default function page() {
           <h2 className="mb-4 mt-2 text-sm text-gray-500">
             Create your account
           </h2>
-          <form action={""} className="w-full">
-            <div className="grid gap-2 py-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                name="email"
-                id="email"
-                placeholder="you@example.com"
-                required
-                // defaultValue={state.values?.email}
-              />
-              <div className="w-full ">
-                <p className="text-xs text-red h-4">
-                  {/* {state?.errors && state.errors.email} */}
-                </p>
-              </div>
-            </div>
-
-            <div className="grid gap-2 py-2">
-              <Label htmlFor="username">UserName</Label>
-              <Input
-                name="username"
-                id="username"
-                placeholder="your username"
-                required
-                // defaultValue={state.values?.username}
-              />
-              <div className="w-full ">
-                <p className="text-xs text-red h-4">
-                  {/* {state?.errors && state.errors.username} */}
-                </p>
-              </div>
-            </div>
-
-            <div className="grid gap-2 py-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                name="password"
-                id="password"
-                placeholder="your password"
-                type="password"
-                required
-                className="my-0"
-                // defaultValue={state.values?.password}
-              />
-              <div className="w-full ">
-                <p className="text-xs text-red h-4">
-                  {/* {state?.errors && state.errors.password} */}
-                </p>
-              </div>
-            </div>
-
-            <Button
-              type="submit"
-              className="bg-cyan text-white w-full"
-              // disabled={isPending}
-            >
-              {/* {isPending ? "Submitting..." : "Submit"} */}
-              Submit
-            </Button>
-          </form>
+          <SignupForm />
         </div>
       </div>
 
---
+++ components/SignupForm.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React, { useActionState, useEffect } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { signUpHandler } from "@/actions/signup";
+
+export default function SignupForm() {
+  const router = useRouter();
+
+  const [state, formAction, isPending] = useActionState(signUpHandler, {
+    success: false,
+    errors: {},
+    values: { email: "", username: "", password: "" },
+  });
+
+  useEffect(() => {
+    if (state.success) {
+      router.push("/");
+    }
+  }, [state.success, router]);
+  return (
+    <form action={formAction} className="w-full">
+      <div className="grid gap-2 py-2">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          name="email"
+          id="email"
+          placeholder="you@example.com"
+          required
+          defaultValue={state.values?.email}
+        />
+        <div className="w-full ">
+          <p className="text-xs text-red h-4">
+            {state?.errors && state.errors.email}
+          </p>
+        </div>
+      </div>
+
+      <div className="grid gap-2 py-2">
+        <Label htmlFor="username">UserName</Label>
+        <Input
+          name="username"
+          id="username"
+          placeholder="your username"
+          required
+          defaultValue={state.values?.username}
+        />
+        <div className="w-full ">
+          <p className="text-xs text-red h-4">
+            {state?.errors && state.errors.username}
+          </p>
+        </div>
+      </div>
+
+      <div className="grid gap-2 py-2">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          name="password"
+          id="password"
+          placeholder="your password"
+          type="password"
+          required
+          className="my-0"
+          defaultValue={state.values?.password}
+        />
+        <div className="w-full ">
+          <p className="text-xs text-red h-4">
+            {state?.errors && state.errors.password}
+          </p>
+        </div>
+      </div>
+
+      <Button
+        type="submit"
+        className="bg-cyan text-white w-full"
+        disabled={isPending}
+      >
+        {isPending ? "Submitting..." : "Submit"}
+      </Button>
+    </form>
+  );
+}
