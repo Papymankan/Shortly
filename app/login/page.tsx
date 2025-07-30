39959bd55555a57cb31a4b96fec159aa7304c906
Papymankan <parsa84rostami@gmail.com>
2025-07-30 12:15:28 +0330

    add login logic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,7 @@
 import Header from "@/components/Header";
 import React from "react";
-
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
 import Footer from "@/components/Footer";
+import LoginForm from "@/components/LoginForm";
 
 export default function page() {
   return (
@@ -17,50 +14,7 @@ export default function page() {
           <h2 className="mb-4 mt-2 text-sm text-gray-500">
             Sign in to your account
           </h2>
-          <form action={""} className="w-full">
-            <div className="grid gap-2 py-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                name="email"
-                id="email"
-                placeholder="you@example.com"
-                required
-                // defaultValue={state.values?.email}
-              />
-              <div className="w-full ">
-                <p className="text-xs text-red h-4">
-                  {/* {state?.errors && state.errors.email} */}
-                </p>
-              </div>
-            </div>
-
-            <div className="grid gap-2 py-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                name="password"
-                id="password"
-                placeholder="your password"
-                type="password"
-                required
-                className="my-0"
-                // defaultValue={state.values?.password}
-              />
-              <div className="w-full ">
-                <p className="text-xs text-red h-4">
-                  {/* {state?.errors && state.errors.password} */}
-                </p>
-              </div>
-            </div>
-
-            <Button
-              type="submit"
-              className="bg-cyan text-white w-full"
-              // disabled={isPending}
-            >
-              {/* {isPending ? "Submitting..." : "Submit"} */}
-              Submit
-            </Button>
-          </form>
+          <LoginForm />
         </div>
       </div>
 
